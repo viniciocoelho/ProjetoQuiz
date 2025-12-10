b7d32cc774a7056e36027cc97873eb01ef67ff0e
viniciocoelho <coelhocvcg@gmail.com>
2025-12-09 23:33:55 -0300

    Atualização da interface, data, type e state
    Feita a interface, o type question, o data Question para formatar a pergunta e o state currentQuestion para controlar a quantidade de perguntas no quiz.

---
+++ app/data/questions.ts
@@ -0,0 +1,15 @@
+import { Questions } from "../types/Questions";
+
+export const questions: Questions[] = [
+    {
+        question: 'O que não pode faltar?',
+        options: [
+            'Farinha',
+            'Corante',
+            'Agua',
+            'Açucar'
+        ],
+        answer: 1
+    }
+
+];
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,29 @@
 "use client"
 
+import { useState } from "react";
+import { questions } from "./data/questions";
+
 
 const Page = () => {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const title = 'Quiz de Culinária';
 
 
   return (
 
-    <div> XXX </div>
+    <div className="w-full h-screen flex justify-center items-center bg-blue-600">
+      <div className="w-full max-w-xl rounded-md bg-white text-black shadow shadow-black">
+        <div className="p-5 font-bold text-2xl border-b border-gray-300">{title}</div>
+        <div className="p-5">
+          XXX
+        </div>
+        <div className="p-5 text-center border-t border-gray-300">
+          {currentQuestion + 1} de {questions.length} pergunta{questions.length === 1 ? '' : 's'}
+        </div>
+
+      </div>
+    
+    </div>
 
 
       

511949d429225282d79827829fabfbfbb76f1aaf
viniciocoelho <coelhocvcg@gmail.com>
2025-12-14 07:51:28 -0300

    Atualização
    Feita a estilização dos campos e a funcionalidade de receber a resposta correta ou errada.

---
+++ app/components/QuestinItem.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { Questions } from "../types/Questions";
+
+type Props = {
+    question: Questions;
+    count: number;
+    onAnswer: (answer: number) =>void;
+}
+
+
+export const QuestionItem = ({question, count, onAnswer}: Props) => {
+
+    const [selectedAnswer, setSelectedAnswer] = useState<number | null>(null);
+
+    const checkQuestion = (key:number) => {
+            if(selectedAnswer === null) {
+                setSelectedAnswer(key);
+                onAnswer(key);
+            }
+    }
+
+    return (
+        <div>
+            <div className="text-3xl font-bold mb-5">{count}. {question.question}</div>
+
+            <div>
+                {question.options.map((item, key) => (
+                    <div
+                        key={key}
+                        onClick={()=> checkQuestion(key)}
+                        className={`border px-3 py-2 rounded-md text-lg mb-4  bg-blue-100 border-blue-300 
+                        
+                        ${selectedAnswer !== null ? 'cursor-auto hover:opacity-100' : 'cursor-pointer hover:opacity-60'}
+                        ${selectedAnswer !== null && selectedAnswer !== question.answer && selectedAnswer === key && 'bg-red-100 border-red-300'}
+                        ${selectedAnswer !== null && selectedAnswer === question.answer && selectedAnswer === key && 'bg-green-100 border-green-300'}
+                        
+                        `}
+                    >{item} 
+                    </div>
+                ))}
+            </div>
+
+        </div>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@
 
 import { useState } from "react";
 import { questions } from "./data/questions";
+import { QuestionItem } from "./components/QuestinItem";
 
 
 const Page = () => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const title = 'Quiz de Culinária';
 
+  const handleAnswered = (answer: number) => { }
 
   return (
 
@@ -15,7 +17,11 @@ const Page = () => {
       <div className="w-full max-w-xl rounded-md bg-white text-black shadow shadow-black">
         <div className="p-5 font-bold text-2xl border-b border-gray-300">{title}</div>
         <div className="p-5">
-          XXX
+          <QuestionItem 
+              question={questions[currentQuestion]}
+              count={currentQuestion + 1}
+              onAnswer={handleAnswered}
+          />
         </div>
         <div className="p-5 text-center border-t border-gray-300">
           {currentQuestion + 1} de {questions.length} pergunta{questions.length === 1 ? '' : 's'}
